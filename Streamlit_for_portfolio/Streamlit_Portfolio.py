0b0525899c7c82be2249abd15006b8b60c2d7f2a
Peter Henry <19714633+Peter95501@users.noreply.github.com>
2024-09-25 13:17:36 -0500

    creating border around conclusion

--- Streamlit_for_portfolio/Streamlit_Portfolio.py
+++ Streamlit_for_portfolio/Streamlit_Portfolio.py
@@ -660,15 +660,19 @@ def digit_rec():
     with col1:
         st.write("The submission achieved an impressive public score of 0.99389, placing it among the top performers in the leaderboard. This score reflects the model's ability to accurately predict handwritten digits from the test set provided by Kaggle.")
     with col2:
-        st.image("Digit_Recognizer/pics/Digit_Recognizer_Code_Result_Accuracy.png", caption="Kaggle Submission Result")
+        st.image("Digit_Recognizer/pics/Digit_Recognizer_Code_Result_Accuracy.png")
         submission = pd.read_csv('Digit_Recognizer/output/submission_CNN_Digit_Recognizer_2024-09-24_15-00-07.csv') 
 
     add_orange_line()
 
-    st.html("<h2 style='text-align: center;'>Conclusion</h2>")
-    st.html("<h5 style='text-align: center;'>This project has demonstrated the power of deep learning in recognizing handwritten digits with remarkable accuracy, achieving a score of 99.39% on Kaggle. However, there is always room for improvement, especially in addressing subtle misclassifications. By fine-tuning the model, increasing data augmentation, or exploring advanced architectures, we can push this accuracy even higher. Whether you are an aspiring data scientist or a seasoned professional, challenges like these present the perfect opportunity to sharpen your skills, dive deeper into machine learning, and contribute to the exciting world of AI-driven solutions. Now is the time to take action to explore, experiment, and innovate!</h5>")    
-
-    add_orange_line()
+    st.markdown("""
+                    <div style='border: 2px solid orange; padding: 20px; margin-top: 10px; text-align: center;'>
+                        <h2 style='text-align: center;'>Conclusion</h2>
+                        <h5 style='text-align: center;'>
+                            This project has demonstrated the power of deep learning in recognizing handwritten digits with remarkable accuracy, achieving a score of 99.39% on Kaggle. However, there is always room for improvement, especially in addressing subtle misclassifications. By fine-tuning the model, increasing data augmentation, or exploring advanced architectures, we can push this accuracy even higher. Whether you are an aspiring data scientist or a seasoned professional, challenges like these present the perfect opportunity to sharpen your skills, dive deeper into machine learning, and contribute to the exciting world of AI-driven solutions. Now is the time to take action to explore, experiment, and innovate!
+                        </h5>
+                    </div>
+                    """, unsafe_allow_html=True)
 
     col1, col2 = st.columns([1,1])
     with col1:
